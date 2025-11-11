9c4367fb8e65f6f10ba000df1d07b7de4c394c4b
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-11 19:50:23 +0600

    ceil() function example

---
+++ 40.ceil() function example.c
@@ -0,0 +1,15 @@
+// ceil() function example
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x,result;
+    printf("Enter a number x: ");
+    scanf("%lf",&x);
+
+    result = ceil(x);
+    printf("ceil(%.2lf) = %.2lf\n",x,result);
+
+    return 0;
+}
