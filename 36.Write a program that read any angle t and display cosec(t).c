0e52a239fc9d6b608b4a123824945d6ae957d26c
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-11 20:05:29 +0600

    Write a program that read any angle t and display cosec(t)

--- 36.Write a program that read any angle t and display cosec(t).c
+++ 36.Write a program that read any angle t and display cosec(t).c
@@ -1,15 +1,15 @@
-// Write a program that read any angle t and display tan(t)
+// Write a program that read any angle t and display cosec(t)
 
 #include <stdio.h>
-#include <math.h>
+#include <math.h>  
 int main()
 {
     double t, result;
     printf("Enter an angle t (in radians): ");
     scanf("%lf", &t);
 
-    result = tan(t);
-    printf("tan(%.2lf) = %.2lf\n", t, result);
+    result = 1 / sin(t);
+    printf("cosec(%.2lf) = %.2lf\n", t, result);
 
     return 0;
 }
