06d7faef25d8ba2d32d9759d536dc016bc15a7a6
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-11 19:16:32 +0600

    Write a program that read any number  x and display log(x)

---
+++ 29.Write a program that read any number  x and display log(x).c
@@ -0,0 +1,14 @@
+// Write a program that read any number  x and display log(x)
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x, result;
+    printf("Enter a number x: ");
+    scanf("%lf", &x);
+
+    result = log(x);
+    printf("log(%.2lf) = %.2lf\n", x, result);
+
+    return 0;
+}
