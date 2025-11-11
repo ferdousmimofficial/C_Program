0165b54d002a2aa7ba6ea004de52f7daa65782c7
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-11 19:50:54 +0600

    floor() function example

---
+++ 41.floor() function example.c
@@ -0,0 +1,15 @@
+// floor() function example
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x,result;
+    printf("Enter a number x: ");
+    scanf("%lf",&x);
+
+    result = floor(x);
+    printf("floor(%.2lf) = %.2lf\n",x,result);
+
+    return 0;
+}
