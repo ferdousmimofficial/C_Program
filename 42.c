396b98728fe90575a7b3cd4d8d5fe3f9ada0563a
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-11 20:30:47 +0600

    Assignment Operator

---
+++ 42.c
@@ -0,0 +1,24 @@
+// Assignment Operator 
+
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    int b=10;
+    int c=15;
+    int d=20;
+
+    a+=2;
+    printf("a = %d\n", a);
+
+    b-=3;
+    printf("b = %d\n", b);  
+
+    c*=4;
+    printf("c = %d\n", c);
+
+    d/=5;
+    printf("d = %d\n", d);
+
+    return 0;
+}
