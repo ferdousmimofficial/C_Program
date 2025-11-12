553309857d4dffe4da028fec7c5a59c98eacf03c
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-13 01:36:02 +0600

    C Program to Check Whether a Character is Vowel or Consonant

---
+++ 52.c
@@ -0,0 +1,18 @@
+// C Program to Check Whether a Character is Vowel or Consonant
+
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any letter: ");
+    scanf("%c", &ch);
+
+    if(ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u" ||
+       ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U"){
+        printf("Vowel");
+    }else{
+        printf("Consonant");
+    }
+
+    return 0;
+}
