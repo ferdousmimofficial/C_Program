cca0fa4efd9dd0a7ad3904fe3517396f6e320a1d
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-13 01:21:05 +0600

    C program to find if a letter is small case or capital case

---
+++ 51.c
@@ -0,0 +1,19 @@
+// C program to find if a letter is small case or capital case
+
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any letter: ");
+    scanf("%c", %ch);
+
+    if(ch >= "a" && ch <= "z"){
+        printf("Small letter");
+    }else if(ch >= "A" && ch <= "Z"){
+        printf("Capital letter");
+    }else{
+        printf("Not a letter");
+    }
+
+    return 0;
+}
