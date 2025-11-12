940468fdcb6b9c4e91efca2af5b210d3f539118d
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-12 20:39:18 +0600

    Check whether the time is morning or not using if...else statement

---
+++ 45.c
@@ -0,0 +1,16 @@
+// Check whether the time is morning or not using if...else statement
+
+#include <stdio.h>
+int main()
+{
+    int time=10;
+    if(time == 10){
+        printf("Good morning.\n");
+        printf("Have a nice day.");
+    }else{
+        printf("Sorry, Its not morning.\n");
+        printf("Have a nice day.");
+    }
+
+    return 0;
+}
