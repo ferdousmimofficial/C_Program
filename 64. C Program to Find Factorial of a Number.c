1f7641a133601f394641edb1954da18a59b2bd16
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-14 02:16:51 +0600

    C Program to Find Factorial of a Number

---
+++ 64. C Program to Find Factorial of a Number.c
@@ -0,0 +1,17 @@
+// C Program to Find Factorial of a Number
+
+#include<stdio.h>
+int main()
+{
+    int n, i, fact=1;
+
+    printf("Enter any positive number: ");
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++){
+        fact = fact * i;
+    }
+    printf("%d",fact);
+
+    return 0;
+}
