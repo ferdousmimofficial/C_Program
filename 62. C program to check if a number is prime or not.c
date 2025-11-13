9ea05e923643645b74a7cdc5af805b40d8d622f3
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-13 22:33:40 +0600

    C program to check if a number is prime or not

---
+++ 62. C program to check if a number is prime or not.c
@@ -0,0 +1,30 @@
+// C program to check if a number is prime
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num, i, count=0;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    if(num <= 1){
+        count++;
+    }
+
+    for(i=2; i<=sqrt(num); i++){
+        if(num % i == 0){
+            count++;
+            break;
+        }
+    }
+
+    if(count == 0){
+        printf("%d is a prime number.\n", num);
+    }else{
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return
+}
