42f73184438260ca809b3ac96bfc1a6476510767
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-16 04:21:07 +0600

    display the series (2 4 6 .... n)

---
+++ 76. display the series (2 4 6 .... n).c
@@ -0,0 +1,16 @@
+// display the series (2 4 6 .... n)
+
+#include<stdio.h>
+int main()
+{
+    int n, i;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    for(i=2; i<=n; i=i+2){
+        printf("%d ",i);
+    }
+
+    return 0;
+}
