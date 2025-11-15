fd88a3fa34d3270315c628ef18a52a7d2f06d039
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-16 04:49:30 +0600

    Series (1^2 + 2^2 + 3^2 + ..... + n^2)Series (1^2 + 2^2 + 3^2 + ..... + n^2)

---
+++ 79. Series (1^2 + 2^2 + 3^2 + ..... + n^2).c
@@ -0,0 +1,18 @@
+// Series (1^2 + 2^2 + 3^2 + ..... + n^2)
+
+#include<stdio.h>
+int main()
+{
+    int n, i, sum=0;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++){
+        sum = sum + i*i;
+    }
+
+    printf("1^2 + 2^2 + ..... + %d^2 = %d\n",n,sum);
+
+    return 0;
+}
