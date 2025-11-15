aa6bef44e7c0b199161d37580cbb3eb053740e7e
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-16 04:31:37 +0600

    Armstrong number

---
+++ 69. Armstrong number.c
@@ -0,0 +1,27 @@
+// Armstrong number
+
+#include <stdio.h>
+
+int main()
+{
+    int num, temp, r, sum = 0;
+
+    printf("Enter any number : ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0)
+    {
+        r = temp % 10;
+        sum = sum + (r * r * r);   // cube of each digit
+        temp = temp / 10;
+    }
+
+    if (num == sum)
+        printf("Armstrong number");
+    else
+        printf("Not Armstrong number");
+
+    return 0;
+}
