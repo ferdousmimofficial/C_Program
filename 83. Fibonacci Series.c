f034e90aadc5fb52dfa8d856a9d09dde094f3ba7
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-16 06:03:20 +0600

    Fibonacci Series

---
+++ 83. Fibonacci Series.c
@@ -0,0 +1,29 @@
+// Fibonacci Series
+
+#include <stdio.h>
+int main()
+{
+    int n, fibo, first = 0, second = 1, count = 0;
+
+    printf("Enter Range: ");
+    scanf("%d", &n);
+
+    while (count < n)
+    {
+        if (count <= 1)
+        {
+            fibo = count;
+        }
+        else
+        {
+            fibo = first + second;
+            first = second;
+            second = fibo;
+        }
+
+        printf("%d ",fibo);
+        count++;
+    }
+
+    return 0;
+}
