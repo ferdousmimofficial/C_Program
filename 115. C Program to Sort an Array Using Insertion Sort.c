e38388edc15a600c908e474d0fde42b7da17c061
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-18 01:43:46 +0600

    C Program to Sort an Array Using Insertion Sort

---
+++ 115. C Program to Sort an Array Using Insertion Sort.c
@@ -0,0 +1,33 @@
+// C Program to Sort an Array Using Insertion Sort
+
+#include <stdio.h>
+
+int main() {
+    int n, key, j;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        // Move elements greater than key to one position ahead
+        while(j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+
+    printf("Sorted array:\n");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
