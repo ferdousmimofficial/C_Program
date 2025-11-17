bb8f09c89038fdb16807a77d1b222b76719f5704
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-18 02:34:37 +0600

    C Program for Decimal to Binary Conversion

---
+++ 162.C Program for Decimal to Binary Conversion.c
@@ -0,0 +1,29 @@
+// C Program for Decimal to Binary Conversion
+
+#include <stdio.h>
+
+int main() {
+    int decimal;
+    int binary[32], i = 0;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
+
+    if(decimal == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+
+    // Convert decimal to binary
+    while(decimal > 0) {
+        binary[i++] = decimal % 2;
+        decimal /= 2;
+    }
+
+    printf("Binary: ");
+    for(int j = i - 1; j >= 0; j--)
+        printf("%d", binary[j]);
+    printf("\n");
+
+    return 0;
+}
