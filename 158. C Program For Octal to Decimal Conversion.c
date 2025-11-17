5ea1b2ef7fc4c750aa07a5370c3efe651780fb8e
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-18 02:30:37 +0600

    C Program For Octal to Decimal Conversion

---
+++ 158. C Program For Octal to Decimal Conversion.c
@@ -0,0 +1,17 @@
+// C Program For Octal to Decimal Conversion
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char octal[20];
+    int decimal;
+
+    printf("Enter an octal number: ");
+    scanf("%s", octal);
+
+    decimal = strtol(octal, NULL, 8); // Convert octal to decimal
+
+    printf("Decimal equivalent: %d\n", decimal);
+    return 0;
+}
