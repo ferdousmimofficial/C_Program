9ff5d7bcc051eef3736995fda140d7b97fa647bb
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-17 07:50:16 +0600

    C Program to Print Inverted Pyramid

---
+++ 88. C Program to Print Inverted Pyramid.c
@@ -0,0 +1,24 @@
+// C Program to Print Inverted Pyramid
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--) {
+        // Print spaces before stars
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for(j = 1; j <= (2*i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
