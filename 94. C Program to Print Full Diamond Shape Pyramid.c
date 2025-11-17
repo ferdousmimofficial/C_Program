b29034f4174ada0cdebd89799aa2cf6ae4676f1d
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-17 08:00:15 +0600

    C Program to Print Full Diamond Shape Pyramid

---
+++ 94. C Program to Print Full Diamond Shape Pyramid.c
@@ -0,0 +1,26 @@
+// C Program to Print Full Diamond Shape Pyramid
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n;
+
+    printf("Enter number of rows for the upper pyramid: ");
+    scanf("%d", &n);
+
+    // Upper pyramid
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n - i; j++) printf(" ");
+        for(j = 1; j <= 2*i - 1; j++) printf("*");
+        printf("\n");
+    }
+
+    // Lower inverted pyramid
+    for(i = n-1; i >= 1; i--) {
+        for(j = 1; j <= n - i; j++) printf(" ");
+        for(j = 1; j <= 2*i - 1; j++) printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
