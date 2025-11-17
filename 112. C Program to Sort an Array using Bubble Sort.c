a643d9c408cd6efc41829eb2ebadc03dd5eb9060
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-18 01:41:25 +0600

    C Program to Sort an Array using Bubble Sort

---
+++ 112. C Program to Sort an Array using Bubble Sort.c
@@ -0,0 +1,34 @@
+// C Program to Sort an Array using Bubble Sort
+
+#include <stdio.h>
+
+int main() {
+    int n, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Bubble Sort
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                // Swap
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    // Print sorted array
+    printf("Sorted array:\n");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
