308a016821baa8a1ff97640642a910095b6d5feb
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-18 02:24:40 +0600

    C Program for Double to String Conversion

---
+++ 153. C Program for Double to String Conversion.c
@@ -0,0 +1,16 @@
+// C Program for Double to String Conversion
+
+#include <stdio.h>
+
+int main() {
+    double num;
+    char str[50];
+
+    printf("Enter a double number: ");
+    scanf("%lf", &num);
+
+    sprintf(str, "%lf", num); // Convert double to string
+
+    printf("Double as string: %s\n", str);
+    return 0;
+}
