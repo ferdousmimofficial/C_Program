2f4a472dd327fb7a16108d82757eb99071c0fd0c
Zannatul Ferdous Mim <ferdousmim.official@gmail.com>
2025-11-18 02:40:02 +0600

    C Program to Swap Two Numbers

---
+++ 167. C Program to Swap Two Numbers.c
@@ -0,0 +1,18 @@
+// C Program to Swap Two Numbers
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Swap
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
